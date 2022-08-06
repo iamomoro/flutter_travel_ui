07a33b30b2274e8c37a08d3547406247ed9ce71e
Elvis Omoro <iamomoroit@gmail.com>
2022-08-06 23:18:30 +0300

    init _selector

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,6 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedIndex = 0;
   
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
