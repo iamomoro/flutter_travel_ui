964dee07d80d8339776c8a09f270742b2ff15aba
Elvis Omoro <iamomoroit@gmail.com>
2022-09-11 22:38:17 +0300

    start activities listing

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_travel_ui/models/activity_model.dart';
 import 'package:flutter_travel_ui/models/destination_model.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -17,87 +18,131 @@ class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        Stack(children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30.0),
-              boxShadow: [BoxShadow(
-                color: Colors.black26, 
-                offset: Offset(0.0, 2.0), 
-                blurRadius: 6.0,
-               ),
-               ],
-              ),
-              child: Hero(
-                tag: widget.destination.imageUrl,
-                child: ClipRRect(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30.0),
-                  child: Image(
-                    image: AssetImage(widget.destination.imageUrl),
-                    fit: BoxFit.cover,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0,
                     ),
+                  ],
+                ),
+                child: Hero(
+                  tag: widget.destination.imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image(
+                      image: AssetImage(widget.destination.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: Icon(Icons.arrow_back),
-                  iconSize: 30.0,
-                  color: Colors.black,
-                  ),
-                  Row(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0, vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
                     IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: Icon(Icons.search),
-                    iconSize: 30.0,
-                    color: Colors.black,
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30.0,
+                      color: Colors.black,
                     ),
-                    IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: Icon(FontAwesomeIcons.sortAmountDown),
-                    iconSize: 25.0,
-                    color: Colors.black,
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(Icons.search),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                        ),
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(FontAwesomeIcons.sortAmountDown),
+                          iconSize: 25.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 20.0,
+                bottom: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.destination.city,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.2),
                     ),
-                  ],)
-                ],
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 15.0,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          widget.destination.country,
+                          style:
+                              TextStyle(color: Colors.white70, fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
+              Positioned(
+                right: 20.0,
+                bottom: 20.0,
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.white70,
+                  size: 25.0,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.destination.activities.length,
+              itemBuilder: (BuildContext context, int index) {
+                Activity activity = widget.destination.activities[index];
+                return Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                      height: 170.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    )
+                  ],
+                );
+              },
             ),
-            Positioned(
-              left: 20.0,
-              bottom: 20.0,
-              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                Text(widget.destination.city, style: TextStyle(color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.w600, letterSpacing: 1.2)),
-                                Row(
-                                  children: <Widget>[
-                                    Icon(FontAwesomeIcons.locationArrow, size: 15.0, color: Colors.white,),
-                                    SizedBox(width: 5.0,),
-                                    Text(widget.destination.country, style: TextStyle(color: Colors.white70, fontSize: 20.0)),
-                                  ],
-                                ),
-                                ],
-                               ),
-            ),
-            Positioned(
-              right: 20.0,
-              bottom: 20.0,
-              child: Icon(
-              Icons.location_on,
-              color: Colors.white70,
-              size: 25.0,
-              ),
-              ),
+          ),
         ],
-        )
-      ],
       ),
     );
   }
